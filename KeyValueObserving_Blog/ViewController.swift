e8836d33cd8a553deddac98f4c9600ce03464605
Erica Millado <ericamillado@gmail.com>
2017-07-26 11:00:55 -0400

    Final

--- KeyValueObserving_Blog/ViewController.swift
+++ KeyValueObserving_Blog/ViewController.swift
@@ -4,22 +4,38 @@
 //
 //  Created by Erica Millado on 1/18/17.
 //  Copyright © 2017 Erica Millado. All rights reserved.
-//
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var seaWebView: WKWebView!
+    let keyPathToObserve:String = "estimatedProgress"
+    let urlString = "https://unsplash.com/search/sealife"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.seaWebView = WKWebView()
+        self.view = seaWebView
+        self.seaWebView.addObserver(self, forKeyPath: keyPathToObserve, options: .new, context: nil)
+        loadWebPage()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadWebPage() {
+        guard let unwrappedURL = URL(string: urlString) else { return }
+        let urlRequest = URLRequest(url: unwrappedURL)
+        self.seaWebView.load(urlRequest)
     }
-
-
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == keyPathToObserve {
+            print("Loaded \(self.seaWebView.estimatedProgress*100)%")
+        }
+    }
+    
+    deinit {
+        self.seaWebView.removeObserver(self, forKeyPath: keyPathToObserve, context: nil)
+    }
 }
 
